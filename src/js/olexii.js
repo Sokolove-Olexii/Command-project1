4324ff023778b7525580db8c528df8efd59df43d
Sokolove-Olexii <130235909+Sokolove-Olexii@users.noreply.github.com>
2025-03-22 06:26:57 -0400

    now

--- src/js/olexii.js
+++ src/js/olexii.js
@@ -1,1 +1,23 @@
-console.log('hello Olexii');
+import axios from 'axios';
+// console.log('hello Olexii');
+const div = document.querySelector('.post');
+export async function getPosts() {
+  const get = await axios.get('http://localhost:3000/posts');
+  createHtml(get.data);
+}
+
+getPosts();
+
+export function createHtml(posts) {
+  const markup = posts
+    .map(item => {
+      return `
+      <h2>${item.nickname}</h2>
+      <p>${item.email}</p>
+      <p>${item.message}</p>
+    `;
+    })
+    .join('');
+
+  div.insertAdjacentHTML('beforeend', markup);
+}
