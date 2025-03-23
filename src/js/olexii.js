9611ac7737071aad8b839e1d9dd229cc7a356c3f
Sokolove-Olexii <130235909+Sokolove-Olexii@users.noreply.github.com>
2025-03-23 05:04:28 -0400

    push

--- src/js/olexii.js
+++ src/js/olexii.js
@@ -1,20 +1,29 @@
 import axios from 'axios';
-// console.log('hello Olexii');
+
 const div = document.querySelector('.post');
 export async function getPosts() {
-  const get = await axios.get('http://localhost:3000/posts');
-  createHtml(get.data);
+  try {
+    const get = await axios.get('http://localhost:3000/posts');
+    createHtml(get.data);
+  } catch (error) {
+    console.log(error.message);
+  }
 }
-
-getPosts();
+document.addEventListener('DOMContentLoaded', () => {
+  getPosts();
+});
 
 export function createHtml(posts) {
   const markup = posts
     .map(item => {
       return `
+    <article>
       <h2>${item.nickname}</h2>
       <p>${item.email}</p>
       <p>${item.message}</p>
+      <button class="update" id="${item.id}">update</button>
+      <button class="delete" id="${item.id}">delete</button>
+    </article>
     `;
     })
     .join('');
